637ccc283d72da183fb9644c92c4482ff37105d2
Jx <JxJxxJxJ@github.com>
2024-10-20 20:00:09 -0300

    add NPRIO, p->times_chosen to process state

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,6 +82,10 @@ struct trapframe {
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
+
+// Prioridades de la MLFQ, un proceso puede tener prioridad 0 hasta NPRIO-1
+#define NPRIO 3
+
 struct proc {
   struct spinlock lock;
 
@@ -91,6 +95,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  uint64 times_chosen;         // Times the scheduler chose the process
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
